4b4d52e043da20f48d2b9dda91ee6ed65d136f83
Sebastien Puigrenier <sebastien.puigrenier@gmail.com>
2022-05-31 23:18:23 +0200

    mobil navbar OK, burger OK

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import "@style/NavBar.css";
+
+import logo from "@assets/SocialTeamConsultingLogo.ico";
 
 function NavBar() {
-  return <h1>Ceci est la NavBar</h1>;
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const handleisMenuVisible = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+  return (
+    <nav className={`${isMenuVisible ? "navbar-visible" : "navbar-hiden"}`}>
+      <div>
+        <img
+          className="navbar-logo"
+          src={logo}
+          alt="logo de la Social Team Consulting"
+        />
+      </div>
+      <h1>Social Team Consulting</h1>
+
+      <div
+        className="navbar-burger"
+        role="button"
+        onClick={() => {
+          handleisMenuVisible();
+        }}
+        tabIndex={0}
+      >
+        <span className="navbar-bar" />
+      </div>
+
+      <div className="navbar-menu_wrapper">
+        <NavLink to="/">
+          <div
+            role="button"
+            tabIndex={0}
+            className="navbar-button"
+            onClick={() => handleisMenuVisible()}
+          >
+            <h1>Devenir intervenant partenaire</h1>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div
+            role="button"
+            tabIndex={0}
+            className="navbar-button"
+            onClick={() => handleisMenuVisible()}
+          >
+            <h1>Devenir association partenaire</h1>
+          </div>
+        </NavLink>
+        <form action="" method="post" className="navbar-form_members">
+          <div className="form-example">
+            <label htmlFor="email">
+              Email :
+              <input type="text" name="name" id="name" required />
+            </label>
+          </div>
+          <div className="form-example">
+            <label htmlFor="mdp">
+              Mot de passe :
+              <input type="email" name="email" id="email" required />
+            </label>
+          </div>
+          <div className="form-example">
+            <input type="submit" value="Subscribe!" />
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
