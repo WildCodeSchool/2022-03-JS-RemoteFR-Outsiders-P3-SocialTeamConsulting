e38e241b162da6d063432a82fe3958826515e069
kpeset <kevin.peset@gmail.com>
2022-06-27 12:00:35 +0200

    Formulaire du front fonctionnel

--- frontend/src/pages/FormAssoContact.jsx
+++ frontend/src/pages/FormAssoContact.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "@style/Form.css";
+import { notifySuccess } from "@services/services";
 
 function FormAssoContact() {
+  const [messageValue, setMessageValue] = useState({
+    nom: "",
+    email: "",
+    telephone: "",
+    message: "",
+  });
+
+  const handleSubmitMessage = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8080/messages/", {
+        nom: messageValue.nom,
+        email: messageValue.email,
+        telephone: messageValue.telephone,
+        message: messageValue.message,
+      })
+      .then(
+        notifySuccess(
+          "Votre message a bien été envoyé. Un administrateur vous contactera bientôt."
+        )
+      );
+  };
+
+  const handleChangeMessage = (event) => {
+    setMessageValue({
+      ...messageValue,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div className="register_asso">
       <div className="back">
-        <form action="#">
+        <form onSubmit={handleSubmitMessage}>
           <div className="register_form">
             <h1>
               Vous êtes une association et vous <br />
@@ -15,26 +48,49 @@ function FormAssoContact() {
             <div>
               <label htmlFor="asso_name">
                 <p>Nom de votre association</p>
-                <input type="text" id="asso_name" />
+                <input
+                  type="text"
+                  id="asso_name"
+                  name="nom"
+                  value={messageValue.nom}
+                  onChange={handleChangeMessage}
+                />
               </label>
             </div>
             <div>
               <label htmlFor="asso_email">
                 <p>Email</p>
-                <input type="email" id="asso_email" />
+                <input
+                  type="email"
+                  name="email"
+                  id="asso_email"
+                  value={messageValue.email}
+                  onChange={handleChangeMessage}
+                />
               </label>
             </div>
             <div>
               <label htmlFor="asso_tel">
                 <p>Téléphone</p>
-                <input type="text" id="asso_tel" />
+                <input
+                  type="text"
+                  id="asso_tel"
+                  name="telephone"
+                  value={messageValue.telephone}
+                  onChange={handleChangeMessage}
+                />
               </label>
             </div>
             <div className="form_textarea">
               <label htmlFor="message">
                 <p>Votre message</p>
 
-                <textarea id="message" />
+                <textarea
+                  id="message"
+                  name="message"
+                  value={messageValue.message}
+                  onChange={handleChangeMessage}
+                />
               </label>
             </div>
             <div className="submit_button">
