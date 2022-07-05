20525dbecca8568f264961904b69f515ea26f97a
kpeset <kevin.peset@gmail.com>
2022-07-05 20:55:43 +0200

    reduction du code

--- frontend/src/components/NavBarBackOfficeLinks.jsx
+++ frontend/src/components/NavBarBackOfficeLinks.jsx
@@ -9,84 +9,28 @@ function NavBarBackOfficeLinks({ handleisMenuVisible, showLink }) {
   if (infoUser.role === undefined) {
     return <div>No way !</div>;
   }
-  if (infoUser.role === "association") {
-    return (
-      <div>
-        <h1>TEST</h1>
-        {DataLinksIntervenants.filter((r) => r.association).map((l) => {
-          return (
-            <div>
-              <NavLink to={l.link}>
-                <div
-                  role="button"
-                  tabIndex={0}
-                  className="navbar-button"
-                  onClick={() => {
-                    handleisMenuVisible(false);
-                    showLink(false);
-                  }}
-                >
-                  <h2>{l.section}</h2>
-                </div>
-              </NavLink>
+  return (
+    <div>
+      <h1>TEST</h1>
+      {DataLinksIntervenants.filter((r) => r[infoUser.role]).map((el) => (
+        <div>
+          <NavLink to={el.link}>
+            <div
+              role="button"
+              tabIndex={0}
+              className="navbar-button"
+              onClick={() => {
+                handleisMenuVisible(false);
+                showLink(false);
+              }}
+            >
+              <h2>{el.section}</h2>
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-  if (infoUser.role === "intervenant") {
-    return (
-      <div>
-        <h1>TEST</h1>
-        {DataLinksIntervenants.filter((r) => !r.association).map((l) => {
-          return (
-            <div>
-              <NavLink to={l.link}>
-                <div
-                  role="button"
-                  tabIndex={0}
-                  className="navbar-button"
-                  onClick={() => {
-                    handleisMenuVisible(false);
-                    showLink(false);
-                  }}
-                >
-                  <h2>{l.section}</h2>
-                </div>
-              </NavLink>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-  if (infoUser.role !== "association" && infoUser.role !== "intervenant") {
-    return (
-      <div>
-        <h1>TEST</h1>
-        {DataLinksIntervenants.map((l) => {
-          return (
-            <div>
-              <NavLink to={l.link}>
-                <div
-                  role="button"
-                  tabIndex={0}
-                  className="navbar-button"
-                  onClick={() => {
-                    handleisMenuVisible(false);
-                    showLink(false);
-                  }}
-                >
-                  <h2>{l.section}</h2>
-                </div>
-              </NavLink>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+          </NavLink>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default NavBarBackOfficeLinks;
