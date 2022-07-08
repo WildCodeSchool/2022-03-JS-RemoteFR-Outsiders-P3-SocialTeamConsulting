0aeb09d1c39fa6445366e8cb588cc254cda4f437
froustex <stephane-feroussier_student2022@wilder.school>
2022-07-08 11:56:30 +0200

    css update users and state

--- frontend/src/components/BackOfficeListeUsers.jsx
+++ frontend/src/components/BackOfficeListeUsers.jsx
@@ -28,56 +28,61 @@ function BackOfficeListeUsers() {
   }, []);
 
   return (
-    <div>
-      <h2> Liste des intervenants</h2>
-      {intervenants.map((intervenant) => {
-        return (
-          <div
-            role="navigation"
-            onClick={() =>
-              navigate("/back_office/modification_profil_intervenant", {
-                state: { email: intervenant.email },
-              })
-            }
-          >
-            <p
-              className={`${intervenant.etat}`}
-            >{` - ${intervenant.nom} ${intervenant.prenom}`}</p>
-          </div>
-        );
-      })}
+    <div className="usersList">
+      <div>
+        <h2 className="listTitle"> Liste des intervenants</h2>
+        {intervenants.map((intervenant) => {
+          return (
+            <div
+              role="navigation"
+              onClick={() =>
+                navigate("/back_office/modification_profil_intervenant", {
+                  state: { email: intervenant.email },
+                })
+              }
+            >
+              <p
+                className={`${intervenant.etat}`}
+              >{` - ${intervenant.nom} ${intervenant.prenom}`}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h2 className="listTitle"> Liste des associations</h2>
+        {associations.map((association) => {
+          return (
+            <div
+              role="navigation"
+              onClick={() =>
+                navigate("/back_office/modification_profil_association", {
+                  state: { email: association.email },
+                })
+              }
+            >
+              <p className={`${association.etat}`}>{` - ${association.nom}`}</p>
+            </div>
+          );
+        })}
+      </div>
 
-      <h2> Liste des associations</h2>
-      {associations.map((association) => {
-        return (
-          <div
-            role="navigation"
-            onClick={() =>
-              navigate("/back_office/modification_profil_association", {
-                state: { email: association.email },
-              })
-            }
-          >
-            <p className={`${association.etat}`}>{` - ${association.nom}`}</p>
-          </div>
-        );
-      })}
-
-      <h2> Liste des administrateurs</h2>
-      {administrateurs.map((administrateur) => {
-        return (
-          <div
-            role="navigation"
-            onClick={() =>
-              navigate("/back_office/modification_profil_administrateur", {
-                state: { email: administrateur.email },
-              })
-            }
-          >
-            <p>{`${administrateur.nom} ${administrateur.prenom}`}</p>
-          </div>
-        );
-      })}
+      <div>
+        <h2 className="listTitle"> Liste des administrateurs</h2>
+        {administrateurs.map((administrateur) => {
+          return (
+            <div
+              role="navigation"
+              onClick={() =>
+                navigate("/back_office/modification_profil_administrateur", {
+                  state: { email: administrateur.email },
+                })
+              }
+            >
+              <p>{`${administrateur.nom} ${administrateur.prenom}`}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
