b7de49ebc09053e4ce272cd53522ed93feebf1fd
Basile Carle <basile.carle@edhec.com>
2022-07-13 12:50:05 +0200

    les filtres de temps et de status fonctionnent correctement et de façon cumulative avec mise à jour du résultat correcte. vérifier fonctionnement pour les missions sur plusieurs mois + affichage de tout l'historique non fonctionnel

--- frontend/src/components/HistoryMissions.jsx
+++ frontend/src/components/HistoryMissions.jsx
@@ -8,6 +8,7 @@ function HistoryMissions() {
   const { infoUser } = useContext(ExportContext.Context);
   const [user, setUser] = useState();
   const [update, setUpdate] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const ENDPOINTINTERVENANT = "/intervenants";
@@ -30,46 +31,151 @@ function HistoryMissions() {
   const [isRefused, setIsRefused] = useState(false);
   const [isValidated, setIsValidated] = useState(true);
   const [isPending, setIsPending] = useState(true);
+  const [isTimeConstrained, setIsTimeConstrained] = useState("");
   const [filter, setFilter] = useState({
     refus: isRefused,
     encours: isPending,
     valide: isValidated,
+    periode: isTimeConstrained,
   });
+
+  const currentDate = new Date();
+  const currentMonth = currentDate.getUTCMonth();
+  const currentYear = currentDate.getUTCFullYear();
+  const monthLowerBoundary = new Date(currentYear, currentMonth, 1, 0, 0, 0);
+  const monthUpperBoundary = new Date(
+    currentYear,
+    currentMonth + 1,
+    0,
+    0,
+    0,
+    0
+  );
+  const lastMonthLowerBoundary = new Date(
+    currentYear,
+    currentMonth - 1,
+    1,
+    0,
+    0,
+    0
+  );
+  const lastMonthUpperBoundary = new Date(
+    currentYear,
+    currentMonth,
+    0,
+    0,
+    0,
+    0
+  );
+  const monthBeforeLastLowerBoundary = new Date(
+    currentYear,
+    currentMonth - 2,
+    1,
+    0,
+    0,
+    0
+  );
+  const monthBeforeLastUpperBoundary = new Date(
+    currentYear,
+    currentMonth - 1,
+    0,
+    0,
+    0,
+    0
+  );
 
   useEffect(() => {
     setFilter({
       refus: isRefused,
       encours: isPending,
       valide: isValidated,
+      periode: isTimeConstrained,
     });
     setUpdate(!update);
-  }, [isRefused, isPending, isValidated]);
+  }, [isRefused, isPending, isValidated, isTimeConstrained]);
 
   useEffect(() => {
     const ENDPOINT = `/missions/history/${user}`;
-    api
-      .get(ENDPOINT)
-      .then((res) => {
-        if (filter.encours && filter.valide && filter.refus) {
-          setMissions(res.data);
-        } else if (!filter.encours && !filter.valide && !filter.refus) {
-          setMissions([]);
-        } else if (filter.encours && filter.valide) {
-          setMissions(res.data.filter((mission) => mission.isvalidated < 2));
-        } else if (filter.encours && filter.refus) {
-          setMissions(res.data.filter((mission) => mission.isvalidated !== 1));
-        } else if (filter.valide && filter.refus) {
-          setMissions(res.data.filter((mission) => mission.isvalidated !== 0));
-        } else if (filter.refus) {
-          setMissions(res.data.filter((mission) => mission.isvalidated === 2));
-        } else if (filter.encours) {
-          setMissions(res.data.filter((mission) => mission.isvalidated === 0));
-        } else if (filter.valide) {
-          setMissions(res.data.filter((mission) => mission.isvalidated === 1));
-        }
-      })
-      .catch((err) => console.error(err));
+
+    const loading = async () => {
+      setIsLoading(true);
+    };
+
+    loading().then(() => {
+      api
+        .get(ENDPOINT)
+        .then((res) => {
+          if (filter.encours && filter.valide && filter.refus) {
+            setMissions(res.data);
+          } else if (!filter.encours && !filter.valide && !filter.refus) {
+            setMissions([]);
+          } else if (filter.encours && filter.valide) {
+            setMissions(res.data.filter((mission) => mission.isvalidated < 2));
+          } else if (filter.encours && filter.refus) {
+            setMissions(
+              res.data.filter((mission) => mission.isvalidated !== 1)
+            );
+          } else if (filter.valide && filter.refus) {
+            setMissions(
+              res.data.filter((mission) => mission.isvalidated !== 0)
+            );
+          } else if (filter.refus) {
+            setMissions(
+              res.data.filter((mission) => mission.isvalidated === 2)
+            );
+          } else if (filter.encours) {
+            setMissions(
+              res.data.filter((mission) => mission.isvalidated === 0)
+            );
+          } else if (filter.valide) {
+            setMissions(
+              res.data.filter((mission) => mission.isvalidated === 1)
+            );
+          }
+        })
+        .then(() => {
+          console.error("les missions ont été mises à jour");
+          setIsLoading(false);
+        })
+        .catch((err) => console.error(err));
+    });
   }, [user, update]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (filter.periode === "this-month") {
+        setMissions(
+          missions.filter(
+            (mission) =>
+              (new Date(mission.date_debut) >= monthLowerBoundary ||
+                new Date(mission.date_fin) >= monthLowerBoundary) &&
+              (new Date(mission.date_debut) <= monthUpperBoundary ||
+                new Date(mission.date_fin) <= monthUpperBoundary)
+          )
+        );
+      } else if (filter.periode === "previous-month") {
+        setMissions(
+          missions.filter(
+            (mission) =>
+              (new Date(mission.date_debut) >= lastMonthLowerBoundary ||
+                new Date(mission.date_fin) >= lastMonthLowerBoundary) &&
+              (new Date(mission.date_debut) <= lastMonthUpperBoundary ||
+                new Date(mission.date_fin) <= lastMonthUpperBoundary)
+          )
+        );
+      } else if (filter.periode === "even-before") {
+        setMissions(
+          missions.filter(
+            (mission) =>
+              (new Date(mission.date_debut) >= monthBeforeLastLowerBoundary ||
+                new Date(mission.date_fin) >= monthBeforeLastLowerBoundary) &&
+              (new Date(mission.date_debut) <= monthBeforeLastUpperBoundary ||
+                new Date(mission.date_fin) <= monthBeforeLastUpperBoundary)
+          )
+        );
+      }
+    }
+  }, [isLoading]);
 
   const handleAnnulationMission = (e) => {
     const ENDPOINTANNULATION = `/accepte/annulation/${e.target.value}/${user}`;
@@ -127,85 +233,8 @@ function HistoryMissions() {
   };
 
   const handleMonthFilter = (e) => {
-    console.error(missions);
-    const currentDate = new Date();
-    const currentMonth = currentDate.getUTCMonth();
-    const currentYear = currentDate.getUTCFullYear();
-    const monthLowerBoundary = new Date(currentYear, currentMonth, 1, 0, 0, 0);
-    const monthUpperBoundary = new Date(
-      currentYear,
-      currentMonth + 1,
-      0,
-      0,
-      0,
-      0
-    );
-    const lastMonthLowerBoundary = new Date(
-      currentYear,
-      currentMonth - 1,
-      1,
-      0,
-      0,
-      0
-    );
-    const lastMonthUpperBoundary = new Date(
-      currentYear,
-      currentMonth,
-      0,
-      0,
-      0,
-      0
-    );
-    const monthBeforeLastLowerBoundary = new Date(
-      currentYear,
-      currentMonth - 2,
-      1,
-      0,
-      0,
-      0
-    );
-    const monthBeforeLastUpperBoundary = new Date(
-      currentYear,
-      currentMonth - 1,
-      0,
-      0,
-      0,
-      0
-    );
-    console.error(lastMonthLowerBoundary);
-    console.error(lastMonthUpperBoundary);
-
-    // get date and month 2022-6-22
-    if (e.target.value === "this-month") {
-      setMissions(
-        missions.filter(
-          (mission) =>
-            (new Date(mission.date_debut) >= monthLowerBoundary ||
-              new Date(mission.date_fin) >= monthLowerBoundary) &&
-            (new Date(mission.date_debut) <= monthUpperBoundary ||
-              new Date(mission.date_fin) <= monthUpperBoundary)
-        )
-      );
-    } else if (e.target.value === "previous-month") {
-      setMissions(
-        missions.filter(
-          (mission) =>
-            (new Date(mission.date_debut) >= lastMonthLowerBoundary ||
-              new Date(mission.date_fin) >= lastMonthLowerBoundary) &&
-            (new Date(mission.date_debut) <= lastMonthUpperBoundary ||
-              new Date(mission.date_fin) <= lastMonthUpperBoundary)
-        )
-      );
-    } else if (e.target.value === "even-before") {
-      setMissions(
-        missions.filter(
-          (mission) =>
-            (new Date(mission.date_debut) >= monthBeforeLastLowerBoundary ||
-              new Date(mission.date_fin) >= monthBeforeLastLowerBoundary) &&
-            (new Date(mission.date_debut) <= monthBeforeLastUpperBoundary ||
-              new Date(mission.date_fin) <= monthBeforeLastUpperBoundary)
-        )
-      );
+    if (e.target.value !== "all-months") {
+      setIsTimeConstrained(e.target.value);
     }
   };
 
@@ -300,9 +329,12 @@ function HistoryMissions() {
               name="month-selection"
               onChange={handleMonthFilter}
             >
-              <option value="this-month">mois en cours</option>
-              <option value="previous-month">mois précédent</option>
-              <option value="even-before">mois - 2</option>
+              <option value="this-month">Mois en cours</option>
+              <option value="previous-month">Mois précédent</option>
+              <option value="even-before">Mois - 2</option>
+              <option value="all-months" selected>
+                Tout l'historique
+              </option>
             </select>
             <p className="inline"> : Validées </p>
           </label>
