134ab848f2845bc2f22ad86c7fcb857423a51422
Basile Carle <basile.carle@edhec.com>
2022-07-25 21:15:28 +0200

    reusinage du système de filtre. reste encore à charger par défault le filtre temporel demandé

--- frontend/src/components/HistoryMissions.jsx
+++ frontend/src/components/HistoryMissions.jsx
@@ -1,18 +1,27 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { api, notifySuccess } from "@services/services";
 import MissionSynthesis from "./MissionSynthesis";
 import "@style/ValidatedMissions.css";
 import ExportContext from "../contexts/Context";
 
 function HistoryMissions() {
+  console.error("debut application");
   const { infoUser } = useContext(ExportContext.Context);
   const [user, setUser] = useState();
   const [update, setUpdate] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [updateRefusedFilter, setUpdateRefusedFilter] = useState(false);
+  const [updatePendingFilter, setUpdatePendingFilter] = useState(false);
+  const [updateValidatedFilter, setUpdateValidatedFilter] = useState(false);
+  const [updateTimeFilter, setUpdateTimeFilter] = useState(false);
+  const [applyTimeFilter, setApplyTimeFilter] = useState(false);
+  const filterStep = useRef(0);
+  const timeFilterStep = useRef(0);
+
+  const ENDPOINTINTERVENANT = "/intervenants";
 
   useEffect(() => {
-    const ENDPOINTINTERVENANT = "/intervenants";
-
+    // on récupère l'id de l'user pour charger ses missions
+    console.error("on récupère l'ID de l'utilisateur");
     api
       .get(ENDPOINTINTERVENANT)
       .then((res) => {
@@ -28,17 +37,186 @@ function HistoryMissions() {
   }, []);
 
   const [missions, setMissions] = useState([]);
+  const [missionsFiltered, setMissionsFiltered] = useState([]);
+
+  // initialisation des valeurs par défault du filtre
   const [isRefused, setIsRefused] = useState(false);
   const [isValidated, setIsValidated] = useState(true);
   const [isPending, setIsPending] = useState(true);
-  const [isTimeConstrained, setIsTimeConstrained] = useState("");
-  const [filter, setFilter] = useState({
-    refus: isRefused,
-    encours: isPending,
-    valide: isValidated,
-    periode: isTimeConstrained,
-  });
+  const isTimeConstrained = useRef("");
 
+  // on charge les missions de l'utilisateur quand l'id de l'utilisateur est récupéré
+  useEffect(() => {
+    console.error("chargement de missions");
+    if (typeof user !== "undefined") {
+      const ENDPOINT = `/missions/history/${user}`;
+
+      api
+        .get(ENDPOINT)
+        .then((res) => {
+          setMissions(res.data);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [user, update]);
+
+  // les fonctions appellées par l'UI pour activer les filtres par les checkbox
+
+  const handleRefusedFilter = (e) => {
+    if (e.target.checked) {
+      filterStep.current = 6; // les fonctions handle ne doivent pas relancer l'ensemble des filtres. 6 signifie ne rien faire après application du filtre
+      setIsRefused(true);
+      setUpdateRefusedFilter(!updateRefusedFilter);
+    } else {
+      filterStep.current = 6;
+      setIsRefused(false);
+      setUpdateRefusedFilter(!updateRefusedFilter);
+    }
+  };
+
+  const handlePendingFilter = (e) => {
+    if (e.target.checked) {
+      filterStep.current = 6; // les fonctions handle ne doivent pas relancer l'ensemble des filtres. 6 signifie ne rien faire après application du filtre
+      setIsPending(true);
+      setUpdatePendingFilter(!updatePendingFilter);
+    } else {
+      filterStep.current = 6;
+      setIsPending(false);
+      setUpdatePendingFilter(!updatePendingFilter);
+    }
+  };
+
+  const handleValidatedFilter = (e) => {
+    if (e.target.checked) {
+      filterStep.current = 6; // les fonctions handle ne doivent pas relancer l'ensemble des filtres. 6 signifie ne rien faire après application du filtre
+      setIsValidated(true);
+      setUpdateValidatedFilter(!updateValidatedFilter);
+    } else {
+      filterStep.current = 6;
+      setIsValidated(false);
+      setUpdateValidatedFilter(!updateValidatedFilter);
+    }
+  };
+
+  // ici on lance les filtres au premier chargement en s'assurant qu'ils se lancent l'un après que l'autre soit executé. Si l'on arrive ici par le biais de la checkbox, on est redirigé vers le 6
+
+  useEffect(() => {
+    if (missions.length > 0) {
+      console.error("on commence à dispatcher");
+      switch (filterStep.current) {
+        case 0:
+          filterStep.current = 1; // si 0 passe à 1 (on appelle le filtre refused)
+          setUpdateRefusedFilter(!updateRefusedFilter);
+          break;
+        case 2:
+          filterStep.current = 3; // si 2 (refused appliqué) on appelle le filtre Pending)
+          setUpdatePendingFilter(!updatePendingFilter);
+          break;
+        case 4:
+          filterStep.current = 5; // si 2 (pending) passe à 3 (validated)
+          setUpdateValidatedFilter(!updateValidatedFilter);
+          break;
+        case 6:
+          break;
+        default:
+          filterStep.current = 0;
+      }
+      if (filterStep.current === 6) {
+        switch (timeFilterStep.current) {
+          case 0:
+            break; // si timeFilterStep = 0, on ne doit pas appliquer le filtre temporel
+          case 1:
+            timeFilterStep.current = 2;
+            setApplyTimeFilter(!applyTimeFilter); // timeFilterStep = 1 : il faut appliquer le filtre maintenant
+            break;
+          case 2:
+            timeFilterStep.current = 0;
+            break;
+          default:
+            timeFilterStep.current = 0;
+        }
+      }
+    }
+  }, [missions, missionsFiltered, updateTimeFilter]);
+
+  // application du filtre refused
+  useEffect(() => {
+    if (
+      (missions.length > 0 && filterStep.current === 1) ||
+      filterStep.current === 6
+    ) {
+      console.error("chargement filtre refus");
+      if (filterStep.current === 1) {
+        filterStep.current = 2; // on indique le prochain filtre à charger lors du chargement initial
+      }
+      if (isRefused) {
+        setMissionsFiltered([
+          ...missionsFiltered,
+          ...missions.filter((mission) => mission.isvalidated === 2),
+        ]);
+      } else if (!isRefused) {
+        setMissionsFiltered([
+          ...missionsFiltered.filter((mission) => mission.isvalidated !== 2),
+        ]);
+      }
+    }
+  }, [updateRefusedFilter, isRefused]);
+
+  // application du filtre pending
+  useEffect(() => {
+    if (
+      missions.length > 0 &&
+      (filterStep.current === 3 || filterStep.current === 6)
+    ) {
+      console.error("chargement filtre pending");
+      if (filterStep.current === 3) {
+        filterStep.current = 4; // on indique le prochain filtre à charger lors du chargement initial
+      }
+      if (isPending) {
+        setMissionsFiltered([
+          ...missionsFiltered,
+          ...missions.filter((mission) => mission.isvalidated === 0),
+        ]);
+      } else if (!isPending) {
+        setMissionsFiltered([
+          ...missionsFiltered.filter((mission) => mission.isvalidated !== 0),
+        ]);
+      }
+    }
+  }, [updatePendingFilter, isPending]);
+
+  // application du filtre validé
+  useEffect(() => {
+    if (
+      missions.length > 0 &&
+      (filterStep.current === 5 || filterStep.current === 6)
+    ) {
+      console.error("chargement filtre validé");
+      if (filterStep.current === 5) {
+        filterStep.current = 6; // on indique le prochain filtre à charger lors du chargement initial
+      }
+      if (isValidated) {
+        setMissionsFiltered([
+          ...missionsFiltered,
+          ...missions.filter((mission) => mission.isvalidated === 1),
+        ]);
+      } else if (!isValidated) {
+        setMissionsFiltered([
+          ...missionsFiltered.filter((mission) => mission.isvalidated !== 1),
+        ]);
+      }
+    }
+  }, [updateValidatedFilter, isValidated]);
+
+  const handleMonthFilter = (e) => {
+    console.error(e.target.value);
+    isTimeConstrained.current = e.target.value;
+    filterStep.current = 0; // on s'assure d'appliquer le filtre temporel sur les filtres de status déjà existant
+    timeFilterStep.current = 1; // on demande au dispatcher d'appliquer un filtre temporel
+    setMissionsFiltered([]); // on réinitialise missionFiltered ce qui relance le dispatcher
+  };
+
+  // on initialise les dates nécessaires au calcul du filtre temporel
   const currentDate = new Date();
   const currentMonth = currentDate.getUTCMonth();
   const currentYear = currentDate.getUTCFullYear();
@@ -85,99 +263,47 @@ function HistoryMissions() {
   );
 
   useEffect(() => {
-    setFilter({
-      refus: isRefused,
-      encours: isPending,
-      valide: isValidated,
-      periode: isTimeConstrained,
-    });
-    setUpdate(!update);
-  }, [isRefused, isPending, isValidated, isTimeConstrained]);
-
-  useEffect(() => {
-    const ENDPOINT = `/missions/history/${user}`;
-
-    const loading = async () => {
-      setIsLoading(true);
-    };
-
-    loading().then(() => {
-      api
-        .get(ENDPOINT)
-        .then((res) => {
-          if (filter.encours && filter.valide && filter.refus) {
-            setMissions(res.data);
-          } else if (!filter.encours && !filter.valide && !filter.refus) {
-            setMissions([]);
-          } else if (filter.encours && filter.valide) {
-            setMissions(res.data.filter((mission) => mission.isvalidated < 2));
-          } else if (filter.encours && filter.refus) {
-            setMissions(
-              res.data.filter((mission) => mission.isvalidated !== 1)
-            );
-          } else if (filter.valide && filter.refus) {
-            setMissions(
-              res.data.filter((mission) => mission.isvalidated !== 0)
-            );
-          } else if (filter.refus) {
-            setMissions(
-              res.data.filter((mission) => mission.isvalidated === 2)
-            );
-          } else if (filter.encours) {
-            setMissions(
-              res.data.filter((mission) => mission.isvalidated === 0)
-            );
-          } else if (filter.valide) {
-            setMissions(
-              res.data.filter((mission) => mission.isvalidated === 1)
-            );
-          }
-        })
-        .then(() => {
-          console.error("les missions ont été mises à jour");
-          setIsLoading(false);
-        })
-        .catch((err) => console.error(err));
-    });
-  }, [user, update]);
-
-  useEffect(() => {
-    if (!isLoading) {
-      if (filter.periode === "all-months") {
-        setUpdate(!update);
-      } else if (filter.periode === "this-month") {
-        setMissions(
-          missions.filter(
+    if (missionsFiltered.length > 0 && timeFilterStep.current === 2) {
+      // on s' assure que missionFiltered est définie et qu'on est bien passé par le dispatcher
+      console.error("application filtre temporel");
+      if (isTimeConstrained.current === "all-months") {
+        console.error("Rien à changer");
+      } else if (isTimeConstrained.current === "this-month") {
+        console.error(missionsFiltered);
+        setMissionsFiltered([
+          ...missionsFiltered.filter(
             (mission) =>
               (new Date(mission.date_debut) >= monthLowerBoundary ||
                 new Date(mission.date_fin) >= monthLowerBoundary) &&
               (new Date(mission.date_debut) <= monthUpperBoundary ||
                 new Date(mission.date_fin) <= monthUpperBoundary)
-          )
-        );
-      } else if (filter.periode === "previous-month") {
-        setMissions(
-          missions.filter(
+          ),
+        ]);
+      } else if (isTimeConstrained.current === "previous-month") {
+        console.error(missionsFiltered);
+        setMissionsFiltered([
+          ...missionsFiltered.filter(
             (mission) =>
               (new Date(mission.date_debut) >= lastMonthLowerBoundary ||
                 new Date(mission.date_fin) >= lastMonthLowerBoundary) &&
               (new Date(mission.date_debut) <= lastMonthUpperBoundary ||
                 new Date(mission.date_fin) <= lastMonthUpperBoundary)
-          )
-        );
-      } else if (filter.periode === "even-before") {
-        setMissions(
-          missions.filter(
+          ),
+        ]);
+      } else if (isTimeConstrained.current === "even-before") {
+        console.error(missionsFiltered);
+        setMissionsFiltered([
+          ...missionsFiltered.filter(
             (mission) =>
               (new Date(mission.date_debut) >= monthBeforeLastLowerBoundary ||
                 new Date(mission.date_fin) >= monthBeforeLastLowerBoundary) &&
               (new Date(mission.date_debut) <= monthBeforeLastUpperBoundary ||
                 new Date(mission.date_fin) <= monthBeforeLastUpperBoundary)
-          )
-        );
+          ),
+        ]);
       }
     }
-  }, [isLoading]);
+  }, [applyTimeFilter]);
 
   const handleAnnulationMission = (e) => {
     const ENDPOINTANNULATION = `/accepte/annulation/${e.target.value}/${user}`;
@@ -210,34 +336,6 @@ function HistoryMissions() {
     return "";
   };
 
-  const handleRefusedFilter = (e) => {
-    if (e.target.checked) {
-      setIsRefused(true);
-    } else {
-      setIsRefused(false);
-    }
-  };
-
-  const handlePendingFilter = (e) => {
-    if (e.target.checked) {
-      setIsPending(true);
-    } else {
-      setIsPending(false);
-    }
-  };
-
-  const handleValidatedFilter = (e) => {
-    if (e.target.checked) {
-      setIsValidated(true);
-    } else {
-      setIsValidated(false);
-    }
-  };
-
-  const handleMonthFilter = (e) => {
-    setIsTimeConstrained(e.target.value);
-  };
-
   return (
     <>
       <div className="header-mission-synthesis">
@@ -259,12 +357,12 @@ function HistoryMissions() {
         </div>
       </div>
       <div className="filters">
-        <form action="" method="post" className="filter-form" onSubmit="">
+        <form action="" method="post" className="filter-form">
           <label htmlFor="refused">
             {isRefused ? (
               <input
                 type="checkbox"
-                value="refusées"
+                value="refused"
                 id="refused"
                 name="refused"
                 checked
@@ -273,7 +371,7 @@ function HistoryMissions() {
             ) : (
               <input
                 type="checkbox"
-                value="refusées"
+                value="refused"
                 id="refused"
                 name="refused"
                 onChange={handleRefusedFilter}
@@ -334,9 +432,7 @@ function HistoryMissions() {
               <option value="this-month">Mois en cours</option>
               <option value="previous-month">Mois précédent</option>
               <option value="even-before">Mois - 2</option>
-              <option value="all-months" selected>
-                Tout l'historique
-              </option>
+              <option value="all-months">Tout l'historique</option>
             </select>
           </label>
         </form>
@@ -348,7 +444,7 @@ function HistoryMissions() {
             <h2>Il n'y a aucune mission dans votre historique.</h2>
           </div>
         ) : (
-          missions.map((mission) => {
+          missionsFiltered.map((mission) => {
             return (
               <MissionSynthesis
                 mission={mission}
